3a9e91ffd87251917e9bd56a6c91902d999b144e
Calder Coalson <caldercoalson@gmail.com>
2019-09-07 08:52:51 -0700

    Bunch o' cleanup.

--- README.md
+++ README.md
@@ -1,37 +1,11 @@
 # 💾 Rust TempDB
 
-[![Documentation](https://docs.rs/tempdb/badge.svg)](https://docs.rs/tempdb) [![Latest Version](https://img.shields.io/crates/v/tempdb.svg)](https://crates.io/crates/tempdb) [![Build Status](https://api.travis-ci.org/calder/rust-tempdb.svg?branch=master)](https://travis-ci.org/calder/rust-tempdb) [![Coverage Stats](https://codecov.io/gh/calder/rust-tempdb/branch/master/graph/badge.svg)](https://codecov.io/gh/calder/rust-tempdb)
+[![Build Status](https://api.travis-ci.org/calder/rust-tempdb.svg?branch=master)](https://travis-ci.org/calder/rust-tempdb) [![Coverage Stats](https://codecov.io/gh/calder/rust-tempdb/branch/master/graph/badge.svg)](https://codecov.io/gh/calder/rust-tempdb)
 
-Temporary databases for unit testing.
+Temporary databases for unit testing, inspired by [`tempfile`](https://docs.rs/tempfile).
 
-## Usage
-
-```rust
-extern crate postgres;
-extern crate tempdb;
-
-use postgres::Connection;
-use postgres::TlsMode;
-use tempdb::cockroach::TempCockroach;
-
-#[test]
-fn test() {
-    let db = TempCockroach::new().expect("Failed to create DB");
-    let conn =
-        Connection::connect(db.url().as_str(), TlsMode::None).expect("Failed to connect to DB");
-
-    conn.execute("CREATE TABLE users (name VARCHAR)", &[])
-        .expect("Failed to create table");
-    conn.execute("INSERT INTO users (name) VALUES ('Alice'), ('Bob')", &[])
-        .expect("Failed to insert into table");
-    let rows = conn
-        .query("SELECT * FROM users", &[])
-        .expect("Failed to read table");
-    assert_eq!(2, rows.len());
-    assert_eq!("Alice", rows.get(0).get::<&str, String>("name"));
-    assert_eq!("Bob", rows.get(1).get::<&str, String>("name"));
-}
-```
+Crates:
+* [`tempdb_cockroach`](tempdb_cockroach) - Single-node [CockroachDB](https://www.cockroachlabs.com) instances.
 
 ## Contributing
 
--- tempdb_cockroach/src/lib.rs
+++ tempdb_cockroach/src/lib.rs
@@ -4,48 +4,21 @@
 //! # Example
 //!
 //! ```rust
-//! extern crate postgres;
 //! extern crate tempdb_cockroach;
 //!
-//! use postgres::Connection;
-//! use postgres::TlsMode;
 //! use tempdb_cockroach::TempCockroach;
 //!
-//! fn create_users_table(conn: &Connection) {
-//!     conn.execute("CREATE TABLE users (name VARCHAR)", &[])
-//!         .expect("Failed to create table");
-//! }
+//! #[test]
+//! fn test() {
+//!     let db = TempCockroach::new().expect("Failed to create DB");
+//!     println!("Connection string: {}", db.url());
 //!
-//! fn create_users(conn: &Connection) {
-//!     conn.execute("INSERT INTO users (name) VALUES ('Alice'), ('Bob')", &[])
-//!         .expect("Failed to insert into table");
-//! }
-//!
-//! #[cfg(test)]
-//! mod tests {
-//!     use super::*;
-//!
-//!     #[test]
-//!     fn test_create_users() {
-//!         let db = TempCockroach::new().expect("Failed to create DB");
-//!         let conn =
-//!             Connection::connect(db.url().as_str(), TlsMode::None).expect("Failed to connect to DB");
-//!
-//!         create_users_table(&conn);
-//!         create_users(&conn);
-//!
-//!         let rows = conn
-//!             .query("SELECT * FROM users", &[])
-//!             .expect("Failed to read table");
-//!         assert_eq!(2, rows.len());
-//!         assert_eq!("Alice", rows.get(0).get::<&str, String>("name"));
-//!         assert_eq!("Bob", rows.get(1).get::<&str, String>("name"));
-//!     }
+//!     // Cockroach process and data are cleaned up when db goes out of scope.
 //! }
 //! ```
 
 extern crate postgres;
-extern crate tempdir;
+extern crate tempfile;
 
 use std::error::Error;
 use std::process::Child;
@@ -54,7 +27,7 @@ use std::time::Duration;
 
 use postgres::Connection;
 use postgres::TlsMode;
-use tempdir::TempDir;
+use tempfile::TempDir;
 
 /// A temporary CockroachDB instance.
 ///
@@ -80,7 +53,7 @@ impl TempCockroach {
     /// Create a new temporary CockroachDB instance.
     pub fn new() -> Result<Self, Box<Error>> {
         // Create the store directory.
-        let tempdir = TempDir::new("cockroach-data")?;
+        let tempdir = TempDir::new()?;
 
         // Spawn the cockroach subprocess.
         let url_file = tempdir.path().join("url");
